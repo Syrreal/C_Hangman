80cc3e9626d1ca14cc949ce8a9dee815a4a4310c
SDHei <SDHeil4561@gmail.com>
2026-07-14 23:41:04 -0700

    Added start of game logic and moved word picking into class. Word picking is currently broken

--- src/hangman.cpp
+++ src/hangman.cpp
@@ -2,24 +2,81 @@
 #include <string>
 #include <vector>
 #include <random>
-#include <time.h>
+#include <algorithm>
+#include <fstream>
+#include <assert.h>
+#include <iostream>
 
 std::string wordFile = "words.txt";
 
-template <typename T, std::size_t N>
-std::string pickWord() {
-    std::ifstream file(wordFile);
-    static_assert(file.is_open());
-    // Count lines to set max for rand() and reset file pointer
-    auto count = std::count_if(std::istreambuf_iterator<char>{file}, {}, [](char c) { return c == '\n'; });
-    std::fseek(file, 0, SEEK_SET);
-    // Seed and pick a random number
-    srand( (unsinged)time(NULL));
-    int r = rand(0, count);
+class Hangman {
+    private:
+        static Hangman* mGame;
+        char mGuessed[26] = {};
+        char mAvailable[26];
+        char mCorrect;
+        char mIncorrect;
+        char mWordWithCorrectGuesses;
+        std::string mWord = "";
+        int mWrongGuessesLeft = 5;
 
-    std::string word = "";
-    for(; count => 0; count--) {
-        std::getline(file, word, '\n');
-    }
-    return word;
+    public:
+        // Potentially construct with different game types (eg. compound words or phrases)
+        Hangman() {
+            // We should only ever have one instance of the game at a time. If one is already set, return it
+            for(int i = 65; i <= 90; i++) {
+                mAvailable[i-65] = static_cast<char>(i);
+            }
+            mWord = pickWord();
+            char mWordWithCorrectGuesses[(int)mWord.length()] = {};
+            char mCorrect[(int)mWord.length()] = {};
+            char mIncorrect[mWrongGuessesLeft] = {};
+        }
+        // OpenGL keypresses are an enum that correspond to the ASCII code, so we'll pass the keypress as an integer
+        // We return if the guess was correct or not so that we can run an animation
+        bool guessLetter(int letter) {
+            // TODO: implement letter checking
+            return false;
+        };
+        void printWord() {
+            std::cout << "The word is: " << mWord << std::endl;
+        };
+
+    private:
+        std::string pickWord() {
+            std::ifstream file(wordFile);
+            assert(file.is_open());
+            // Count lines to set max for rand()
+            auto count = std::count_if(std::istreambuf_iterator<char>{file}, {}, [](char c) { return c == '\n'; });
+            std::cout << "Line count: " << count << std::endl;
+            // BUG: File iterator is at EOF after counting - need to find a way to reset iterator
+            // May just have to re-open the file
+
+            // std::fseek(std::istreambuf_iterator<char>{file}, 0, SEEK_SET); ## Does not compile with this line, file obj type is wrong
+
+            // Seed and pick a random number, bind it to the range set by count
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_int_distribution<> distrib(0, (int)count);
+            int r = distrib(gen);
+
+            std::cout << "Picking word on line: " << r << std::endl;
+
+            std::string word = "";
+            for(; r >= 0; r--) {
+                std::cout << "On line: " << (count - (count - r)) << std::endl;
+                std::getline(file, word);
+                std::cout << "Current word: " << word << std::endl;
+            }
+            file.close();
+            return word;
+        };
+
 };
+
+
+// TODO: remove after testing
+int main() {
+    Hangman game = Hangman{};
+    game.printWord();
+}
